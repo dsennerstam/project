27bd815a5e2e9b9d8e894ea699e0f669395fff1b
Daniel <daniel@balancedsolutions.se>
2019-02-16 23:24:43 +0100

    Some JS CSS Animation

---
+++ script.js
@@ -0,0 +1,28 @@
+const element =  document.querySelector('.bounce')
+element.classList.add('animated', 'zoomInRight');
+
+
+
+
+
+const element1 =  document.querySelector('.bounce1')
+element1.classList.add('animated', 'zoomIn');
+
+
+const element2 =  document.querySelector('.bounce2')
+element2.classList.add('animated', 'slideInLeft');
+
+const element3 =  document.querySelector('.bounce3')
+element3.classList.add('animated', 'jackInTheBox');
+
+const element4 =  document.querySelector('.bounce4')
+element4.classList.add('animated', 'flipInX');
+
+const element5 =  document.querySelector('.bounce5')
+element5.classList.add('animated', 'flipInX');
+
+const element6 =  document.querySelector('.bounce6')
+element6.classList.add('animated', 'flipInX');
+
+const element7 =  document.querySelector('.bounce7')
+element7.classList.add('animated', 'flipInX');

71cd099c79c1d630e7c6dd8dffe52af56a42495f
Daniel <daniel@balancedsolutions.se>
2019-02-16 23:32:56 +0100

    small changes

--- script.js
+++ script.js
@@ -2,24 +2,21 @@ const element =  document.querySelector('.bounce')
 element.classList.add('animated', 'zoomInRight');
 
 
-
-
-
 const element1 =  document.querySelector('.bounce1')
 element1.classList.add('animated', 'zoomIn');
 
 
 const element2 =  document.querySelector('.bounce2')
-element2.classList.add('animated', 'slideInLeft');
+element2.classList.add('animated', 'jello');
 
 const element3 =  document.querySelector('.bounce3')
 element3.classList.add('animated', 'jackInTheBox');
 
 const element4 =  document.querySelector('.bounce4')
-element4.classList.add('animated', 'flipInX');
+element4.classList.add('animated', 'heartBeat');
 
 const element5 =  document.querySelector('.bounce5')
-element5.classList.add('animated', 'flipInX');
+element5.classList.add('animated', 'heartBeat');
 
 const element6 =  document.querySelector('.bounce6')
 element6.classList.add('animated', 'flipInX');
